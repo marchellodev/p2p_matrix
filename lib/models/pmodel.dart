35c31a0b98b341ef147e17a8686d8e8c086cf094
marchellodev <marchellodev@gmail.com>
2021-02-13 09:02:42 +0200

    ffi: now it works!

--- lib/models/pmodel.dart
+++ lib/models/pmodel.dart
@@ -1,22 +1,23 @@
-import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:math';
 
 import 'package:ffi/ffi.dart';
+import 'package:ffi/ffi.dart' as ffi;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 import 'package:p2p_matrix/components/buttons.dart';
-import 'package:p2p_matrix/models/history.dart';
+import 'package:p2p_matrix/log.dart';
 
 import '../main.dart';
-import 'history.dart';
 
 part 'pmodel.g.dart';
 
-typedef StringFunction = Pointer<Utf8> Function();
+typedef GetNameFunction = Pointer<Utf8> Function();
+typedef RunFunction = Pointer<Void> Function(
+    Pointer<Utf8> json, Pointer<Utf8> path);
 
 @HiveType(typeId: 1)
 class PModel {
@@ -30,7 +31,8 @@ class PModel {
   final DateTime lastModified;
 
   DynamicLibrary _lib;
-  StringFunction getName;
+  GetNameFunction getName;
+  RunFunction run;
 
   PModel({
     @required this.path,
@@ -39,49 +41,69 @@ class PModel {
   }) {
     _lib = DynamicLibrary.open(path);
     getName = _lib
-        .lookup<NativeFunction<StringFunction>>('GetModelName')
-        .asFunction<StringFunction>();
+        .lookup<NativeFunction<GetNameFunction>>('GetModelName')
+        .asFunction<GetNameFunction>();
+
+    run = _lib
+        .lookup<NativeFunction<RunFunction>>('Run')
+        .asFunction<RunFunction>();
   }
 
-  void simulate(String scriptName) {
+  Future<void> simulate(String scriptName) async {
     final rand = Random();
 
     final fileName =
         '${getName().toDartString()}_${scriptName}_${rand.nextInt(100000000)}.json';
 
-    final model = HistoryModel(
-        fileName: fileName,
-        modelName: getName().toDartString(),
-        modelCreated: lastModified,
-        modelSize: size,
-        scriptName: scriptName,
-        scriptNodes: 10000,
-        scriptOperations: 10000,
-        historyDate: DateTime.now(),
-        dataNotFound: rand.nextDouble(),
-        amountOfUsedNodes: HistoryStats(
-          range: rand.nextDouble() * rand.nextInt(100),
-          median: rand.nextDouble() * rand.nextInt(100),
-          average: rand.nextDouble() * rand.nextInt(100),
-          standardDeviation: rand.nextDouble() * rand.nextInt(100),
-        ),
-        timeToAcquireDate: HistoryStats(
-          range: rand.nextDouble() * rand.nextInt(100),
-          median: rand.nextDouble() * rand.nextInt(100),
-          average: rand.nextDouble() * rand.nextInt(100),
-          standardDeviation: rand.nextDouble() * rand.nextInt(100),
-        ),
-        usedMemory: HistoryStats(
-          range: rand.nextDouble() * rand.nextInt(100),
-          median: rand.nextDouble() * rand.nextInt(100),
-          average: rand.nextDouble() * rand.nextInt(100),
-          standardDeviation: rand.nextDouble() * rand.nextInt(100),
-        ));
+    //
+    // final model = HistoryModel(
+    //     fileName: fileName,
+    //     modelName: getName().toDartString(),
+    //     modelCreated: lastModified,
+    //     modelSize: size,
+    //     scriptName: scriptName,
+    //     scriptNodes: 10000,
+    //     scriptOperations: 10000,
+    //     historyDate: DateTime.now(),
+    //     dataNotFound: rand.nextDouble(),
+    //     amountOfUsedNodes: HistoryStats(
+    //       range: rand.nextDouble() * rand.nextInt(100),
+    //       median: rand.nextDouble() * rand.nextInt(100),
+    //       average: rand.nextDouble() * rand.nextInt(100),
+    //       standardDeviation: rand.nextDouble() * rand.nextInt(100),
+    //     ),
+    //     timeToAcquireDate: HistoryStats(
+    //       range: rand.nextDouble() * rand.nextInt(100),
+    //       median: rand.nextDouble() * rand.nextInt(100),
+    //       average: rand.nextDouble() * rand.nextInt(100),
+    //       standardDeviation: rand.nextDouble() * rand.nextInt(100),
+    //     ),
+    //     usedMemory: HistoryStats(
+    //       range: rand.nextDouble() * rand.nextInt(100),
+    //       median: rand.nextDouble() * rand.nextInt(100),
+    //       average: rand.nextDouble() * rand.nextInt(100),
+    //       standardDeviation: rand.nextDouble() * rand.nextInt(100),
+    //     ));
     File('storage/history/$fileName').create(recursive: true);
-    File('storage/history/$fileName')
-        .writeAsStringSync(jsonEncode(model.toJson()));
 
-    updateHistory();
+    // File('storage/history/$fileName')
+    //     .writeAsStringSync(jsonEncode(model.toJson()));
+
+    run(scriptName.toNativeUtf8(),
+        File('storage/history/$fileName').path.toNativeUtf8());
+
+    while (true) {
+      await Future.delayed(const Duration(seconds: 1));
+      final f = await File('storage/history/$fileName').length();
+
+      if (f != 0) {
+        llog('Response from the Go code saved here: storage/history/$fileName');
+
+        updateHistory();
+        return;
+      }
+      llog('Awaiting response from the Go code: 1 second passed');
+    }
   }
 }
 
